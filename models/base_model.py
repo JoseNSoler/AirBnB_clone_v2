c889a917843af23cba4701da204eda25cd1eef2c
JoseNSoler <josenico1212@gmail.com>
2021-09-01 20:12:17 -0500

    [FIX] no print pruebas

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,11 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = self.updated_at = datetime.now()
         storage.new(self)
-        print ("pruebasasas", kwargs)
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
-
                     setattr(self, key, value)
 
 

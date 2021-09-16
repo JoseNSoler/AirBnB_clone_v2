618cb0aa60ee58aade9985a4f9d02f671ce763a0
JoseNSoler <josenico1212@gmail.com>
2021-09-16 01:51:47 -0500

    [UPLOAD] DEPLOY STATIC file 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# Install and update nginx to deploy web_static
+
+sudo apt-get update
+sudo apt-get install -y nginx
+
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+sudo touch /data/web_static/releases/test/index.html
+
+printf %s "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+ubuntu@89-web-01:~/$ curl localhost/hbnb_static/index.html
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" > /data/web_static/releases/test/index.html
+
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+printf %s "server {
+    listen      80 default_server;
+    listen      [::]:80 default_server;
+    root        /etc/nginx/html;
+    index       index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+}
+" > /etc/nginx/sites-available/default
+
+sudo service nginx restart

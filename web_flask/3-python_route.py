737c54b0e5e868a4763769d96e1da835f94c0e9c
JoseNSoler <josenico1212@gmail.com>
2021-09-19 14:28:28 -0500

    [UPLOAD] FLASK DEPLOY file 3 4 5

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Start flask app """
+from flask import Flask, escape
+
+app = Flask('__name__')
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cme(text):
+    text = text.replace('_', ' ')
+    return 'C %s' % escape(text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    text = text.replace('_', ' ')
+    return 'Python %s' % escape(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" Start flask app """
+from flask import Flask, escape, render_template
+
+app = Flask('__name__')
+
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    # Return string on root req
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    # Return string on /hbnb req
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def cme(text):
+    # Return 'C <user_req>'
+    text = text.replace('_', ' ')
+    return 'C %s' % escape(text)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text="is cool"):
+    # Return Python <user_req>
+    text = text.replace('_', ' ')
+    return 'Python %s' % escape(text)
+
+
+@app.route('/number/<n>')
+def isnumber(n):
+    # Return if <user_req_int> is a int
+    if int(n):
+        return "{} is a number".format(n)
+
+
+@app.route('/number_template/<n>')
+def display_html_if_num(n):
+    # Return webpage if <user_req_int> is a int
+    return render_template('5-number.html', n=int(n))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
